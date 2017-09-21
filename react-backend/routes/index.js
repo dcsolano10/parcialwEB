9eee4c40bc49442e5d04f34be3c8d14229a5a95c
Diana Catalina Solano Beltrán <dc.solano10@uniandes.edu.co>
2017-09-21 15:09:07 -0500

    Backend funcional

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -4,43 +4,21 @@ var GitHubApi = require("github");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express testee' });
 });
 
 router.get('/getFollowers/:user', function(req, res, next) {
 
 var github = new GitHubApi({});
-
-constructor(props) {
-    super(props);
-    this.state = {value: ''};
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(event) {
-    // alert('A name was submitted: ' + this.state.value);
-    var upperClass = this;
-    axios.get('/getFollowers/' + this.state.value)
-      .then(function (response) {
-        upperClass.setPlayers(response.data)
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    event.preventDefault();
-  }
-
-  render() {
-    <SearchBox onSearch={this.onSearch.bind(this), user={"req.params.user"}}</SearchBox>
-  }
  
 // TODO: optional authentication here depending on desired endpoints. See below in README.
  //req.params.user 
 github.users.getFollowingForUser({
-    username: "req.params.user"
-}, function(err, data) {
+    username: req.params.user
+  }, function(err, data) {
+    console.log(req.params.user);
     res.json(data);
-});
+  });
 });
 
 module.exports = router;
